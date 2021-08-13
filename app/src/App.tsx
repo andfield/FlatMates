68bfc76d8e3527d8db2400775708766e0554b6f5
Sid Thakur <sid@pop-os.localdomain>
2021-08-13 15:25:57 +1200

    Basic Components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import styled from "styled-components";
+import Body from './components/Body/Body';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      {/* NavBar */}
+      <NavBar />
+
+      {/* Body */}
+      <Body />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  &&&{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+`;
