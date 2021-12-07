5d027234b343c71ef31cd6bc41ef08130336e68f
andfield <siddharththakur76@gmail.com>
2021-12-07 21:49:04 +1300

    I don't know what I did

---
+++ app/pages/api/_middleware.ts
@@ -0,0 +1,5 @@
+export function middleware(req){
+    const auth = req.headers.get('authorization')
+
+    if (basic)
+}
---
+++ app/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className='flex-1'>
+      <Head>
+        <title>Flat mates</title>
+        <meta name="description" content="A flat management app developed by NerdsParadise." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='flex'>
+
+      </main>
+    </div>
+  );
+};
+
+export default Home;
